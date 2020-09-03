f901db31c42c48551577abe87d5c115c1dcf3e9b
vaibhavsahni009 <vaibhavsahni009@gmail.com>
2020-09-04 04:35:09 +0530

    yargs added

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,54 @@
 const chalk = require('chalk')
 const getNotes=require('./notes.js')
+const yargs=require('yargs')
 
-console.log(getNotes())
-console.log(chalk.green("Success!"))
+yargs.command({
+    command:'add',
+    describe:'add notes',
+    builder:{
+        title:{
+            describe:'note title',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'note body',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:(args)=>{
+        console.log('Title '+args.title)
+        console.log('Body '+args.body)
+
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe:'remove notes',
+    handler:()=>{
+        console.log('removing notes')
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe:'list notes',
+    handler:()=>{
+        console.log('listing notes')
+    }
+})
+
+yargs.command({
+    command:'read',
+    describe:'read notes',
+    handler:()=>{
+        console.log('reading notes')
+    }
+})
+
+
+// console.log(yargs.argv)
+
+yargs.parse()
