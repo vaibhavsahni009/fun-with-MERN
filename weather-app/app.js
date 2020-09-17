2ed2a05990d6de620904522812c2a670bcc5b0c1
vaibhavsahni009 <vaibhavsahni009@gmail.com>
2020-09-18 04:56:03 +0530

    done some es6 stuff also read comment in app.js of weather app
    not planning to use map-box api to get all the strings regarding location
    open weather is enough for cities and states

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,10 +4,14 @@ const openWeather=require('./utils/openWeather.js')
 
 
 
-openWeather('delhi',(err,data)=>{
-
-console.log('Error' ,err)
-console.log('Data',data)
+if(process.argv[2])
+openWeather(process.argv[2],(err,data)=>{
+    // Thought of destructuring data but in case of 
+    // err it will give undefined so might find work around that
+if(err)return console.log(err)
+console.log(data.name)
+console.log(data.forecast)
 
 
 })
+else console.log('Provide a valid city name')
--- weather-app/utils/openWeather.js
+++ weather-app/utils/openWeather.js
@@ -5,10 +5,15 @@ const request = require('request')
 const openWeather=(address,callback)=>{
 
     const url=`https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(address)}&units=metric&appid=1a8c679aec2646f20ff70bb853785c1b`
-    request({url, json:true},(err,res)=>{
+    request({url, json:true},(err,{body})=>{
         if (err)callback('Unable to connect to internet!')
-        else if (res.body.cod!==200)callback(res.body.message)
-        else callback(undefined,`It's currently ${res.body.main.temp} degrees out with some ${res.body.weather[0].description}.`)
+        else if (body.cod!==200)callback(body.message)
+        else 
+        callback(undefined,
+            {
+                name:body.name,
+                forecast:`It's currently ${body.main.temp} degrees out with some ${body.weather[0].description}.`
+            })
 
 
     })
