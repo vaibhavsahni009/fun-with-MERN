4ecf7701b48dd5eb81ba622f133c8f6648522fe0
vaibhavsahni009 <vaibhavsahni009@gmail.com>
2020-09-30 03:38:54 +0530

    Web server with basic styling

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const path = require('path')
+const hbs =require('hbs')
 
 const app=express()
-const publicDir=path.join(__dirname,'../public')
+const publicDirPath=path.join(__dirname,'../public')
+const viewsPath=path.join(__dirname,'../templates/views')
+const partialPath=path.join(__dirname,'../templates/partials')
 
-app.use(express.static(publicDir))
 
+app.use(express.static(publicDirPath))
 
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialPath)
+
+app.get('',(req, res)=>{
+    res.render('index',{
+        title: 'Weather'
+    ,    name:'Ging'
+    
+    })
+})
+
+app.get('/about',(req, res)=>{
+    res.render('about',{
+        title:'About',
+        name:'Ging'
+    })
+})
+
+app.get('/help',(req, res)=>{
+    res.render('help',{
+        title:'Help',
+        help_message:'Well help',
+        name:'Ging'
+
+    })
+})
 
 
 app.get('/weather',(req,res)=>{
@@ -17,6 +47,23 @@ app.get('/weather',(req,res)=>{
 
 })
 
+app.get('/help/*',(req, res)=>{
+    res.render('page_not_found',{
+        title:'404',
+        message:'Help Article not found',
+        name:'Ging'
+
+    })
+})
+
+app.get('*',(req, res)=>{
+    res.render('page_not_found',{
+        title:'404',
+        message:'Page not found',
+        name:'Ging'
+
+    })
+})
 
 app.listen(3000,()=>{
 
