bf556295c3b5d6731fde6ba7f628c19e73bd4844
vaibhavsahni009 <vaibhavsahni009@gmail.com>
2020-12-22 18:03:33 +0530

    jwt and middleware

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -10,7 +10,9 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token= await user.generateAuthToken()
+        
+        res.status(201).send({user,token})
     } catch (error) {
         res.status(400).send(error)
     }
@@ -28,8 +30,8 @@ router.post('/user/login',async (req,res)=>{
     try {
         
         const user=await User.findByCredentials(req.body.email,req.body.password)
-
-        res.send(user)
+        const token= await user.generateAuthToken()
+        res.send({user,token})
     } catch (error) {
         res.status(400).send(error)
     }
