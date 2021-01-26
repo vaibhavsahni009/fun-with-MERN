99fb40cbeaa2b64d2920ebbe191261bdfd25bd35
Vaibhav Sahni <vaibhavsahni009@gmail.com>
2021-01-26 21:36:11 +0530

    deletion and addition of avatar on database

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -4,59 +4,64 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Task = require("./task");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age cannot be negative");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  },
-  email: {
-    unique: true,
-    type: String,
-    required: true,
-    trim: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is not valid");
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      // if(value.length<6){
-      //     throw new Error('Password must have atleast 6 characters')
-      // }
-      // else
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("Password should not contain Password");
-      }
-    },
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age cannot be negative");
+        }
       },
     },
-  ],
-},
-{
-    timestamps:true,
-});
+    email: {
+      unique: true,
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 7,
+      validate(value) {
+        // if(value.length<6){
+        //     throw new Error('Password must have atleast 6 characters')
+        // }
+        // else
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password should not contain Password");
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    avatar: {
+      type: Buffer,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.virtual("tasks", {
   ref: "Task",
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,146 +1,154 @@
-const express = require('express');
-const User =require('../models/user')
-const auth=require('../middleware/auth')
-const multer=require('multer');
-const router= new express.Router()
+const express = require("express");
+const User = require("../models/user");
+const auth = require("../middleware/auth");
+const multer = require("multer");
+const router = new express.Router();
 
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
 
-router.post('/users', async (req, res) => {
-    const user = new User(req.body)
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
 
-    try {
-        await user.save()
-        const token= await user.generateAuthToken()
-        
-        res.status(201).send({user,token})
-    } catch (error) {
-        res.status(400).send(error)
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post("/user/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  // try {
+  //     const users = await User.find({})
+  //     res.send(users)
+  // } catch (error) {
+  //     res.status(500).send(error)
+  // }
+
+  res.send(req.user);
+});
+
+router.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send();
     }
-})
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
-router.post('/user/login',async (req,res)=>{
-    try {
-        
-        const user=await User.findByCredentials(req.body.email,req.body.password)
-        const token= await user.generateAuthToken()
-        res.send({user,token})
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "age", "password", "email"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-router.post('/users/logout',auth,async (req,res)=>{
-    try {
-        
-        req.user.tokens=req.user.tokens.filter((token)=>{
-            return token.token!==req.token
-        })
+  if (!isValidOperation) {
+    return res.status(400).send({
+      errror: "Invalid Updates!",
+    });
+  }
 
-        await req.user.save()
-        res.send()
-    } catch (error) {
-        res.status(500).send()
-    }
-})
+  try {
+    const user = await req.user;
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
 
-
-router.post('/users/logoutAll',auth,async (req,res)=>{
-    try {
-        
-        req.user.tokens=[]
-
-        await req.user.save()
-        res.send()
-    } catch (error) {
-        res.status(500).send()
-    }
-})
-
-router.get('/users/me',auth, async (req, res) => {
-
-    // try {
-    //     const users = await User.find({})
-    //     res.send(users)
-    // } catch (error) {
-    //     res.status(500).send(error)
+    // if (!user) {
+    //     return res.status(404).send()
     // }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
-    res.send(req.user)
-})
+router.delete("/users/me", auth, async (req, res) => {
+  try {
+    // const user = await User.findByIdAndDelete(req.params.id)
+    // if (!user) {
+    //     return res.status(404).send()
+    // }
+    await req.user.remove();
+    res.send(req.user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
-router.get('/users/:id', async (req, res) => {
-
-    try {
-        const user = await User.findById(req.params.id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
-
-router.patch('/users/me',auth, async (req, res) => {
-
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'age', 'password', 'email']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({
-            'errror': 'Invalid Updates!'
-        })
-    }
-
-    try {
-
-        const user = await req.user
-        updates.forEach((update)=>user[update]=req.body[update])
-        await user.save()
-
-        // if (!user) {
-        //     return res.status(404).send()
-        // }
-        res.send(user)
-    } catch (error) {
-        res.status(400).send(error)
+const upload = multer({
+  // dest:'avatars',
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpeg|jpg)$/)) {
+      return cb(new Error("Please upload an image file"));
     }
 
-})
+    cb(undefined, true);
+  },
+});
 
-router.delete('/users/me',auth, async (req, res) => {
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ Error: error.message });
+  }
+);
 
-    try {
-        // const user = await User.findByIdAndDelete(req.params.id)
-        // if (!user) {
-        //     return res.status(404).send()
-        // }
-        await req.user.remove()
-        res.send(req.user)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
+});
 
-const upload=multer({
-    dest:'avatars',
-    limits:{
-        fileSize:1000000
-    },
-    fileFilter(req,file,cb){
-        if(!file.originalname.match(/\.(png|jpeg|jpg)$/)){
-            return cb(new Error('Please upload an image file'))
-        }
-
-        cb(undefined,true)
-    }
-})
-
-router.post('/users/me/avatar',upload.single('avatar'),(req,res)=>{
-    res.send()
-},(error,req,res,next)=>{
-  res.status(400).send({Error:error.message})  
-})
-
-module.exports=router
+module.exports = router;
