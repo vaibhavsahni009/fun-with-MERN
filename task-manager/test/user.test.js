7b3a13af690130fe2fe649656309fc2376717b04
vaibhavsahni009 <vaibhavsahni009@gmail.com>
2021-03-05 16:46:27 +0530

    mock functions for testing

--- task-manager/test/user.test.js
+++ task-manager/test/user.test.js
@@ -19,7 +19,7 @@ beforeEach(async () => {
 });
 
 test("user signup", async () => {
-  await request(app)
+  const response=await request(app)
     .post("/users")
     .send({
       name: "Baibhav",
@@ -27,17 +27,35 @@ test("user signup", async () => {
       password: "MyPass777!",
     })
     .expect(201);
+
+    const user =await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    expect(response.body).toMatchObject({
+        user:{
+            name:'Baibhav',
+            email:'BaibhavSahni009@random.com'
+        },
+        token:user.tokens[0].token
+    })
+
+    expect(user.password).not.toBe('MyPass777!')
+
 });
 
 test("user login", async () => {
-  console.log(userOne.email, userOne.password);
-  await request(app)
+//   console.log(userOne.email, userOne.password);
+  const response=await request(app)
     .post("/user/login")
     .send({
       email: userOne.email,
       password: userOne.password,
     })
     .expect(200);
+
+   const user=await User.findById(userOneId)
+   
+   expect(response.body.token).toBe(user.tokens[1].token)
 });
 
 test("user login wrong credentials", async () => {
@@ -52,10 +70,35 @@ test("user login wrong credentials", async () => {
 });
 
 test("user profile", async () => {
-    console.log(userOne.tokens[0].token)
+    // console.log(userOne.tokens[0].token)
   await request(app)
     .get("/users/me")
     .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
     .send()
     .expect(200);
+});
+
+test("user profile unauthenticated", async () => {
+  await request(app)
+    .get("/users/me")
+    .send()
+    .expect(401);
+});
+
+test("user profile deletion", async () => {
+  await request(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+  const user=await User.findById(userOneId)
+  
+  expect(user).toBeNull()
+});
+
+test("user profile deletion unauthenticated", async () => {
+  await request(app)
+    .delete("/users/me")
+    .send()
+    .expect(401);
 });
